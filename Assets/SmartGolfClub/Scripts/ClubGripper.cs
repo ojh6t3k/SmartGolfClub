386ba74ad7864c00e87eec8846f0af4fdd52e6e9
Jaehong, Oh <jhoh@idealink.kr>
2015-07-03 17:42:37 +0900

    1차 점검
    - 형구씨가 작업한 최신 리소스 추가
    - Step1 완료
    - Step2 완료
    - Step3 작업 중

---
+++ Assets/SmartGolfClub/Scripts/ClubGripper.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SmartGolf
+{
+	[AddComponentMenu("SmartGolf/ClubGripper")]
+	public class ClubGripper : MonoBehaviour
+	{
+		public Transform rightAt;
+		public Transform rightTo1;
+		public Transform rightTo2;
+		public Transform leftAt;
+		public Transform leftTo1;
+		public Transform leftTo2;
+
+		private Animator _animator;
+
+		// Use this for initialization
+		void Start ()
+		{
+			_animator = GetComponent<Animator>();		
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+
+		void OnAnimatorIK(int layerIndex)
+		{
+			if(rightAt != null)
+			{
+				_animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+				_animator.SetIKPosition(AvatarIKGoal.RightHand, rightAt.position);
+				if(rightTo1 != null && rightTo2 != null)
+				{
+					Vector3 forward1 = rightTo1.position - rightAt.position;
+					Vector3 forward2 = rightTo2.position - rightAt.position;
+					Vector3 up = Vector3.Cross(forward2, forward1);
+					_animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
+					_animator.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.LookRotation(forward1, up));
+				}
+			}
+
+			if(leftAt != null)
+			{
+				_animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+				_animator.SetIKPosition(AvatarIKGoal.LeftHand, leftAt.position);
+
+				if(leftTo1 != null && leftTo2 != null)
+				{
+					Vector3 forward1 = leftTo1.position - leftAt.position;
+					Vector3 forward2 = leftTo2.position - leftAt.position;
+					Vector3 up = Vector3.Cross(forward1, forward2);
+					_animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
+					_animator.SetIKRotation(AvatarIKGoal.LeftHand, Quaternion.LookRotation(forward1, up));
+				}
+			}
+		}
+	}
+}
---
+++ Assets/SmartGolfClub/Scripts/ClubPivot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SmartGolf
+{
+	[AddComponentMenu("SmartGolf/ClubPivot")]
+	public class ClubPivot : MonoBehaviour
+	{
+		public Transform root;
+		public Transform toPivot;
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+			if(root != null && toPivot != null)
+			{
+				root.position += (toPivot.position - transform.position);
+			}		
+		}
+	}
+}
